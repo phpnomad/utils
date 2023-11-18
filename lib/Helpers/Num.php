5e30a93a8c75d5daef462ed196f28438f2026d0d
Alex Standiford <alex@standiford.us>
2023-11-18 13:10:40 -0600

    progress

--- lib/Helpers/Num.php
+++ lib/Helpers/Num.php
@@ -13,4 +13,24 @@ class Num
     {
         return $amount * static::toPercentageFloat($percent);
     }
+
+    /**
+     * Divides the specified number, rounds it up or down, and returns the integer.
+     *
+     * @param numeric $amount
+     * @param numeric $dividedBy
+     * @param int $mode
+     * @return int
+     */
+    public static function getDividedInt($amount, $dividedBy, int $mode = PHP_ROUND_HALF_UP): int
+    {
+        $product = $amount / $dividedBy;
+
+        return static::floatToInt($product, $mode);
+    }
+
+    public static function floatToInt(float $input, int $mode = PHP_ROUND_HALF_UP): int
+    {
+        return (int) round($input, 0, $mode);
+    }
 }
